42075afd404b3e090b743ef1c21cbe0807853f3f
Dorian Devois <dorian.devois@gmail.com>
2026-04-16 15:05:20 +0400

    fix data

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,7 +1,74 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-initApp();
+const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+populateGallery();
+const imageGallery = initSimpleLightbox('.js-gallery a');
 
 function imageCardTpl(image) {
   const { preview, original, description } = image;
@@ -25,6 +92,16 @@ function renderGallery(images, element) {
   element.innerHTML = createGalleryMarkup(images);
 }
 
+function populateGallery() {
+  const gallery = document.querySelector('.js-gallery');
+
+  if (!gallery) {
+    return;
+  }
+
+  renderGallery(images, gallery);
+}
+
 function initSimpleLightbox(selector) {
   const lightboxOptions = {
     /* options */
@@ -37,31 +114,4 @@ function initSimpleLightbox(selector) {
   };
 
   return new SimpleLightbox(selector, lightboxOptions);
-}
-
-async function fetchImages() {
-  try {
-    const response = await fetch('../data/images.json');
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch statuses: ${response.status}`);
-    }
-
-    return response.json();
-  } catch (error) {
-    console.error('Error fetching competition statuses:', error);
-    return [];
-  }
-}
-
-async function initApp() {
-  const gallery = document.querySelector('.js-gallery');
-  const images = await fetchImages();
-
-  if (!gallery) {
-    return;
-  }
-
-  renderGallery(images, gallery);
-  const imageGallery = initSimpleLightbox('.gallery-link');
 }
